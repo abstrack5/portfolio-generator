d23fe008c599e4040a64a86da9a088d369b07043
Abstrack <Armon_ah05@outlook.com>
2022-05-03 20:28:31 -0400

    learned how to generate an HTML and seperate code to be cleaner and easier to read

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
 
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
 
-
-
-
-
-
-
-
-
-
-
-
-
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-  };
-  printProfileData(profileDataArgs);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
