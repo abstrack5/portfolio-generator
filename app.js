7ab6181f97c5e002c24dd225ca0aac24f349563b
Abstrack <Armon_ah05@outlook.com>
2022-05-04 23:08:43 -0400

    added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 const inquirer = require("inquirer");
 
 const promptUser = () => {
@@ -139,15 +141,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+      console.log(portfolioData);
+    });
   });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
